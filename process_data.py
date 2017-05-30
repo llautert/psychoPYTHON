b4aca58c50e5a8d53310278178d7ca45c72a69bc
Larissa Lautert <larissalautert@gmail.com>
2017-05-30 16:35:28 -0300

    undo last commits

--- process_data.py
+++ process_data.py
@@ -3,20 +3,19 @@ import numpy as np
 import pandas as pd
 
 def load_processed_data_test():
-    df = pd.read_csv('~/data/dataframe_output2.csv', sep=',')
-    Y = df['lifecycle_stage']
+    df = pd.read_csv("C:\\Users\\rafaela.oliveira\\Desktop\\base_waze.csv", sep=',')
+    Y = df['totaldias']
     
-    X = df[['conversions_sum', 'leads_float']]
-#    X = df[['job_title', 'area_de_atuacao_da_empresa', 'conversions_sum', 'leads_float']]
-#    X['job_title'] = X['job_title'].astype('category')
-#    X['area_de_atuacao_da_empresa'] = X['area_de_atuacao_da_empresa'].astype('category')
+    X = df[['cdTipoCartorio','cdForo','cdVara','cdClasseExt','qtParteAtiva','qtPartePassiva','vlCausa']]
+    X['cdTipoCartorio'] = X['cdTipoCartorio'].astype('category')
+    X['cdForo'] = X['cdForo'].astype('category')
+    X['cdVara'] = X['cdVara'].astype('category')
+    X['cdClasseExt'] = X['cdClasseExt'].astype('category')
     return X, Y
+load_processed_data_test()
 
 def load_predict_data_test():
-    df = pd.read_csv('~/data/dataframe_output2.csv', sep=',')
-    Y = df['lifecycle_stage']
-    X = df[['conversions_sum', 'leads_float']]
-    #X = df[['job_title', 'area_de_atuacao_da_empresa', 'conversions_sum', 'leads_float']]
+    df = pd.read_csv("C:\\Users\\rafaela.oliveira\\Desktop\\base_waze.csv", sep=',')
+    Y = df['totaldias']
+    X = df[['cdTipoCartorio','cdForo','cdVara','cdClasseExt','qtParteAtiva','qtPartePassiva','vlCausa']]
     return X
-
-load_processed_data_test()
