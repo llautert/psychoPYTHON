503e9642729e0af46eaa32120d8d08a75efee6fe
Rafaela Helbing <rafaelahelbing@gmail.com>
2017-07-11 14:49:31 -0300

    Update process_data.py

--- process_data.py
+++ process_data.py
@@ -3,54 +3,33 @@ import numpy as np
 import pandas as pd
 
 
+#import numpy as np
+import pandas as pd
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.preprocessing import LabelEncoder
 
 def load_processed_data_test():
     df = pd.read_csv("data", sep=';')
-    df = df[df['STATUS_PROCESSO']=='BAIXADO']
+    X = df2[['variaveis explicativas']]
+    Y = pd.to_numeric(df2['resposta'])
 
-    Y = df['totaldias'].astype(int)
-    X = df[['Segmento', 'flArea', 'flProcVirtual', 'cdTipoCartorio', 'cdClasseExt','cdAssuntoExt','cdComarca','cdForo','cdVara','cdRaj','nuEntrancia','qtParteAtiva','qtPartePassiva','qtTestemunha', 'qtAudiencia','TemRecurso', 'flReuPreso', 'flPrioridadeIdoso', 'qtMovAposBaixa', 'flJusticaGratuita', 'qtMov', 'TeveAudiencia', 'TeveAcordo', 'qtDocEmitido', 'TemDefensoria', 'TemPerito',  
-    'tpPessoaAtiva', 'tpGovernoAtiva', 'tpPessoaPassiva', 'tpGovernoPassiva', 'cdAgente', 'qtMovMag', 'qtDocEmitidoMag', 'totaldias', 'vlCausa', 'idade_passiva', 'idade_ativa']]
-   
 
-    X['cdTipoCartorio'] = X['cdTipoCartorio'].astype(int).astype('category')
-    X['cdForo'] = X['cdForo'].astype(int).astype('category')
-    X['cdVara'] = X['cdVara'].astype(int).astype('category')
-    X['cdClasseExt'] = X['cdClasseExt'].astype(int).astype('category')
-    X['Segmento'] = X['Segmento'].astype('category')
-    X['flArea'] = X['flArea'].astype('category')
-    X['flProcVirtual'] = X['flProcVirtual'].astype('category')
-    X['cdTipoCartorio'] = X['cdTipoCartorio'].astype(int).astype('category')
-    X['cdClasseExt'] = X['cdClasseExt'].astype(int).astype('category')
-    X['cdComarca'] = X['cdComarca'].astype(int).astype('category')
-    X['cdRaj'] = X['cdRaj'].astype(int).astype('category')
-    X['nuEntrancia'] = X['nuEntrancia'].astype(int).astype('category')
-    X['flJusticaGratuita'] = X['flJusticaGratuita'].astype('category')
-    X['TeveAudiencia'] = X['TeveAudiencia'].astype('category')
-    X['TeveAcordo'] = X['TeveAcordo'].astype('category')
-    X['TemDefensoria'] = X['TemDefensoria'].astype('category')
-    X['TemPerito'] = X['TemPerito'].astype('category')
-    X['tpPessoaAtiva'] = X['tpPessoaAtiva'].astype('category')
-    X['tpGovernoAtiva'] = X['tpGovernoAtiva'].astype('category')
-    X['tpPessoaPassiva'] = X['tpPessoaPassiva'].astype('category')
-    X['tpGovernoPassiva'] = X['tpGovernoPassiva'].astype('category')
-    X['cdAgente'] = X['cdAgente'].astype(int).astype('category')
-    X['idade_passiva'] = X['idade_passiva'].astype('category')
-    X['idade_ativa'] = X['idade_ativa'].astype('category')
-    X.fillna(0)
-    ### transformando variaveis caracteres
-    X = pd.get_dummies(X)
+# tratando variáveis categóricas
+    category_map = {}
+    category_list = ['VARIAVEIS CATEGORIAS']
 
-    #print(Y.dtypes)
-    #print(X.dtypes)
-    #print(X.head())
-    #print(Y.head())
+    # for each category transform into numbers
+    for cat in category_list:
+        encoder = LabelEncoder()
+        X[cat] = encoder.fit_transform(X[cat]) # fitting this category and trasnforming the column to indexes
+        category_map[cat] = encoder.classes_   # saving the indexes to know how to go back from index->category
+
+# gerando matriz de OneHotEncoder
+    # gerando matriz de OneHotEncoder
+    categorical_indexes=[]
+    for cat in category_list:
+        categorical_indexes.append(X.columns.get_loc(cat))
+    one_hot_encoder = OneHotEncoder(categorical_features = categorical_indexes)
+    print(X.head())
+    print(Y.head())
     return X, Y
-load_processed_data_test()
-
-
-#def load_predict_data_test():
-#    df = pd.read_csv("C:\\Users\\rafaela.oliveira\\Desktop\\base_waze.csv", sep=',')
- #   Y = df['totaldias']
- #   X = df[['cdTipoCartorio','cdForo','cdVara','cdClasseExt','qtParteAtiva','qtPartePassiva','vlCausa']]
- #   return X
