ff7aad3d7c3443e7d0c9ddee8e2fa97cad0d4e7a
Rafaela Helbing <rafaelahelbing@gmail.com>
2017-07-18 18:50:15 -0300

    Update models.py

--- models.py
+++ models.py
@@ -14,19 +14,19 @@ from multiprocessing import Pool
 
 def deep_1net(X, y):
     # split data
-    X_train, X_test, Y_train, Y_test = cross_validation.train_test_split(X, y, test_size=0.33, random_state=42)
+    X_train, X_test, y_train, y_test = cross_validation.train_test_split(X, y, test_size=0.33, random_state=42)
 
     # transform to_categorical data
-    Y_test = to_categorical(Y_test)
-    Y_train = to_categorical(Y_train)
+    y_test = to_categorical(y_test)
+    y_train = to_categorical(y_train)
 
     model = Sequential()
     model.add(Dense(units=3, input_dim=2, kernel_initializer='normal', activation='relu'))
     model.compile(loss=mean_absolute_error,
                   optimizer=SGD(lr=0.01),
                   metrics=['accuracy', 'mae'])
-    model.fit(X_train, Y_train, epochs=10, batch_size=30, verbose=1, validation_data=(X_test, Y_test))
-    score = model.evaluate(X_test, Y_test, verbose=1)
+    model.fit(X_train, y_train, epochs=10, batch_size=30, verbose=1, validation_data=(X_test, y_test))
+    score = model.evaluate(X_test, y_test, verbose=1)
     model.save("deep1net.h5")
     print('\nTest loss:', score[0])
     print('Test accuracy:', score[1])
@@ -140,12 +140,12 @@ def select_models(X_train, type_pred, is_labeled_data, is_text_data, is_number_c
 
 
 def train_test_model(args):
-    name, m1, X, y, X_train, Y_train, X_test, Y_test, type_pred, is_number_categories_known, is_labeled_data = args
+    name, m1, X, y, X_train, y_train, X_test, y_test, type_pred, is_number_categories_known, is_labeled_data = args
     print(m1)
     if not is_number_categories_known:
         plot_learning_curve(m1, X, y, name, ylim=(0.7, 1.01))
     # with parallel_backend('distributed', scheduler_host='localhost:8786', scatter=[X_train, Y_train]):
-    m1.fit(X_train, Y_train)
+    m1.fit(X_train, y_train)
     externals.joblib.dump(m1, "%s.pkl" % name, compress=9)
 
     y_pred = m1.predict(X_test)
@@ -153,25 +153,25 @@ def train_test_model(args):
 
     if type_pred == 'category' and is_labeled_data:
         # classification metrics
-        results.write("Accuracy Score: %.2f\n" % (metrics.accuracy_score(Y_test, y_pred)))
-        results.write("F1 Score: %.2f\n" % (metrics.f1_score(Y_test, y_pred, average="weighted")))
-        results.write("Precision Score: %.2f\n" % (metrics.precision_score(Y_test, y_pred, average="weighted")))
-        results.write("Recall Score: %.2f\n" % (metrics.recall_score(Y_test, y_pred, average="weighted")))
+        results.write("Accuracy Score: %.2f\n" % (metrics.accuracy_score(y_test, y_pred)))
+        results.write("F1 Score: %.2f\n" % (metrics.f1_score(y_test, y_pred, average="weighted")))
+        results.write("Precision Score: %.2f\n" % (metrics.precision_score(y_test, y_pred, average="weighted")))
+        results.write("Recall Score: %.2f\n" % (metrics.recall_score(y_test, y_pred, average="weighted")))
     elif type_pred == 'category' and is_number_categories_known:
         # clusterization
-        results.write("Completeness Score: %.2f\n" % (metrics.completeness_score(Y_test, y_pred)))
-        results.write("Homogeneity Score: %.2f\n" % (metrics.homogeneity_score(Y_test, y_pred)))
-        results.write("V-Measure Score: %.2f\n" % (metrics.v_measure_score(Y_test, y_pred)))
+        results.write("Completeness Score: %.2f\n" % (metrics.completeness_score(y_test, y_pred)))
+        results.write("Homogeneity Score: %.2f\n" % (metrics.homogeneity_score(y_test, y_pred)))
+        results.write("V-Measure Score: %.2f\n" % (metrics.v_measure_score(y_test, y_pred)))
         results.write(
-            "Adjusted Mutual Information Score: %.2f\n" % (metrics.adjusted_mutual_info_score(Y_test, y_pred)))
-        results.write("Fowlkes-Mallows index (FMI): %.2f\n" % (metrics.fowlkes_mallows_score(Y_test, y_pred)))
+            "Adjusted Mutual Information Score: %.2f\n" % (metrics.adjusted_mutual_info_score(y_test, y_pred)))
+        results.write("Fowlkes-Mallows index (FMI): %.2f\n" % (metrics.fowlkes_mallows_score(y_test, y_pred)))
     elif type_pred == 'quantity':
         # regression
-        results.write("R2 Score: %.2f\n" % (metrics.r2_score(Y_test, y_pred)))
-        results.write("Explained Variance Score: %.2f\n" % (metrics.explained_variance_score(Y_test, y_pred)))
-        results.write("Mean Absolute Error: %.2f\n" % (metrics.mean_absolute_error(Y_test, y_pred)))
-        results.write("Mean Squared Error: %.2f\n" % (metrics.mean_squared_error(Y_test, y_pred)))
-        results.write("Median Absolute Error: %.2f\n" % (metrics.median_absolute_error(Y_test, y_pred)))
+        results.write("R2 Score: %.2f\n" % (metrics.r2_score(y_test, y_pred)))
+        results.write("Explained Variance Score: %.2f\n" % (metrics.explained_variance_score(y_test, y_pred)))
+        results.write("Mean Absolute Error: %.2f\n" % (metrics.mean_absolute_error(y_test, y_pred)))
+        results.write("Mean Squared Error: %.2f\n" % (metrics.mean_squared_error(y_test, y_pred)))
+        results.write("Median Absolute Error: %.2f\n" % (metrics.median_absolute_error(y_test, y_pred)))
     results.close()
 
 
@@ -180,7 +180,7 @@ def autoscikit(X, y, type_pred='category', is_labeled_data=False, is_text_data=F
     start_time = time()
     models = []
     names = []
-    X_train, X_test, Y_train, Y_test = cross_validation.train_test_split(X, y, test_size=0.33, random_state=42)
+    X_train, X_test, y_train, y_test = cross_validation.train_test_split(X, y, test_size=0.33, random_state=42)
 
     if X_train.shape[0] <= 50:
         print("Error few data")
@@ -193,9 +193,9 @@ def autoscikit(X, y, type_pred='category', is_labeled_data=False, is_text_data=F
     # register_parallel_backend('distributed', DistributedBackend)
     sequence = []
     # cross validation
-    externals.joblib.dump((X, y, X_train, Y_train, X_test, Y_test), "dataset.pkl", compress=9)
+    externals.joblib.dump((X, y, X_train, y_train, X_test, y_test), "dataset.pkl", compress=9)
     for name, m1 in zip(names, models):
-        sequence.append([name, m1, X, y, X_train, Y_train, X_test, Y_test, type_pred, is_number_categories_known,
+        sequence.append([name, m1, X, y, X_train, y_train, X_test, y_test, type_pred, is_number_categories_known,
                          is_labeled_data])
     pool.map(train_test_model, sequence)
     pool.close()
